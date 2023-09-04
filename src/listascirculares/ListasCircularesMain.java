e909a8945cecb42ca404354d24a2b7dcb1aa6fe2
JTTomasCH <141368999+JTTomasCH@users.noreply.github.com>
2023-09-04 15:12:01 -0500

    Add files via upload

---
+++ src/listascirculares/ListasCircularesMain.java
@@ -0,0 +1,68 @@
+
+package listascirculares;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Tomás Chiriví
+ */
+public class ListasCircularesMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ListasCirculares<Persona> personas = new ListasCirculares<Persona>();
+        
+        System.out.println("La lista esta vacia? "+personas.isEmpty()+"\n");
+        Persona per1 = new Persona("Jorge", 15);
+        Persona per2 = new Persona("Tomas", 32);
+        personas.addLast(per1);
+        personas.addLast(per2);
+        System.out.println("Lista 1: ");
+        
+        personas.showList();
+
+        
+        System.out.println("");
+        
+
+        ListasCirculares<Persona> persona = new ListasCirculares<Persona>();
+        Persona per3 = new Persona("Juan", 15);
+        Persona per4 = new Persona("Andres", 32);
+        Persona per5 = new Persona("Carlos", 15);
+        Persona per6 = new Persona("Luisa", 32);
+        Persona per7 = new Persona("Santiago", 20);
+        
+        persona.addLast(per3);
+        persona.addLast(per4);
+        persona.addLast(per5);
+        persona.addLast(per6);
+        persona.addFirst(per7);
+
+        
+        System.out.println("Lista 2: ");
+        
+        persona.showList();
+        System.out.println("");
+
+        
+        Iterator<Persona> counterClockwiseIterator = persona.counterClockwiseIterator();
+        System.out.println("Recorrido en sentido contrario de las manecillas del reloj:");
+        while (counterClockwiseIterator.hasNext()) {
+            System.out.print(counterClockwiseIterator.next() + " ");
+        }
+        System.out.println();
+        System.out.println();
+        
+        
+        Iterator<Persona> clockwiseIterator = persona.clockwiseIterator();
+        System.out.println("Recorrido en sentido de las manecillas del reloj:");
+        while (clockwiseIterator.hasNext()) {
+            System.out.print(clockwiseIterator.next() + " ");
+        }
+        System.out.println();
+    }
+    
+}
